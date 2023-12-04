9e1c0b5c76f42f771469dfe751300944695002d4
Andrej <achren1@gmail.com>
2023-12-04 14:48:39 +0100

    a1

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("backend - fox ticket project");
     }
 
-    publis static void greet(){
+    public static void greet(){
         System.out.println("Andrej greets you all <3");
     }
 }
