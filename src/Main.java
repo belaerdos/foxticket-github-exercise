191ca4d96f36ea23d33d596f5e1c5d54da2b5606
belaerdos <erdosbelamusic@gmail.com>
2023-12-04 14:53:34 +0100

    b1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
 
     public static void project() {
@@ -11,4 +12,5 @@ public class Main {
         System.out.println("Andrej greets you all <3");
     }
 
+}
 }
