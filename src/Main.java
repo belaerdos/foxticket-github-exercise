1d7df8cad7ee53c62f8ffbfb953f3a25331c46fa
chabyie <dominik.cajan1@gmail.com>
2023-12-04 15:21:20 +0100

    c3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         C.cheer("FOXTICKET SAUSAGE TEAM!!!");
+        FutureD.future("Tiger");
     }
-
 
     public static void greet() {
         System.out.println("Greetings!");
