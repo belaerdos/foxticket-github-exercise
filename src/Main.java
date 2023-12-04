0463b7ae649760ad19e58aa242d19cfb03777a89
Štěpán Kostolány <stepan.kostolany@seznam.cz>
2023-12-04 14:28:19 +0100

    e1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+    }
+
+    public static void project() {
+        System.out.println("backend - fox ticket project");
     }
 }
