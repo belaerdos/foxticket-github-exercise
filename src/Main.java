b3c627242f65da9f4dd78c498b1ed296f7157c29
Petr Půta <petr.puta1@gmail.com>
2023-12-04 15:31:06 +0100

    d5

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         C.cheer("FOXTICKET SAUSAGE TEAM!!!");
-        FutureD.future("Tiger");
+        FutureD.future("Tiger", "Beran");
     }
 
     public static void greet() {
