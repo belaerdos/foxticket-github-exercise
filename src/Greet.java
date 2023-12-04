1f77bc6b6eae4ce9022bd41927bf3fa87bd30b3d
belaerdos <erdosbelamusic@gmail.com>
2023-12-04 16:23:13 +0100

    b7
    try again

--- src/Greet.java
+++ src/Greet.java
@@ -2,6 +2,7 @@ public class Greet {
 
     public void greet(String name1, String name2){
         System.out.println("Greetings from" + name1 + "and" + name2);
+        System.out.println("I WANNA TRY AGAIN WOHOOOO");
     }
 
 }
